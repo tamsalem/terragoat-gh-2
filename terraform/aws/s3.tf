db61cd064c3439932fea08a953d0ff605f0f7a45
Tal Amsalem <100066758+tamsalem@users.noreply.github.com>
2022-03-06 13:59:04 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -25,12 +25,6 @@ resource "aws_s3_bucket_object" "data_object" {
     }, {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "a7f01cc7-63c2-41a8-8555-6665e5e39a64"
   })
 }
 
